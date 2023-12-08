c279bd6a60da9e0f5949c73fde6734de87e12947
MatthewTan420 <s10239950@connect.np.edu.sg>
2023-12-08 22:38:33 +0800

    Initial commit

---
+++ Assets/test.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public Animator animator;
+    public Transform door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Key" && gameObject.tag == "Door")
+        {
+            animator.SetBool("isKey",true);
+            collision.transform.SetParent(door);
+        }
+
+        if (collision.gameObject.tag == "Destroy" && gameObject.tag == "Weapon")
+        {
+            Destroy(collision.gameObject);
+        }
+
+        if (gameObject.tag == "Destroy")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
