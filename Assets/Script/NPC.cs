63e7f7491b38de2fc32c5e69906ad8740c66fdbe
Weekiat553 <116060902+Weekiat553@users.noreply.github.com>
2024-01-06 17:07:25 +0800

    Skybox and npc script
    t

--- Assets/Script/NPC.cs
+++ Assets/Script/NPC.cs
@@ -7,6 +7,8 @@ public class NPC : MonoBehaviour
     public Animator animator;
     public float firerate;
     public float nextfire;
+    public float voiceTimer;
+    public AudioSource aiVoice;
 
     // Start is called before the first frame update
     void Start()
@@ -23,10 +25,26 @@ public class NPC : MonoBehaviour
             animator.SetBool("isTalk", true);
             Invoke(nameof(Talking), 3.0f);
         }
+
+ 
     }
 
     private void Talking()
     {
         animator.SetBool("isTalk", false);
     }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            aiVoice.Play();
+            if (voiceTimer > 0)
+            {
+                animator.SetBool("isTalk", false);
+            }
+        }
+        
+    }
+
 }
